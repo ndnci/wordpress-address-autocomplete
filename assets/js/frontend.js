65b6a273c2a935b331d1a44e619cb3bdc0bf59fa
Ahliman HUSEYNOV <contact@ahlimanhuseynov.com>
2025-12-07 19:05:40 +0100

    refactor: Improve suggestions positioning and handling for better UX

--- assets/js/frontend.js
+++ assets/js/frontend.js
@@ -51,6 +51,16 @@
             $(document).on("change", ".ndnci-wpaa-address-field", function () {
                 self.updateMaps();
             });
+
+            // Reposition suggestions on scroll/resize
+            $(window).on("scroll resize", function () {
+                if (self.activeField && self.activeField.length) {
+                    var $suggestions = self.getSuggestionsContainer(self.activeField);
+                    if ($suggestions.is(":visible")) {
+                        self.positionSuggestions(self.activeField, $suggestions);
+                    }
+                }
+            });
         },
 
         /**
@@ -68,6 +78,15 @@
             }
 
             var $suggestions = this.getSuggestionsContainer($field);
+
+            if ($suggestions.length === 0) {
+                console.error("[WPAA] No suggestions container found!");
+                return;
+            }
+
+            // Position suggestions relative to field
+            this.positionSuggestions($field, $suggestions);
+
             $suggestions.html('<div class="wpaa-loading">' + ndnciWpaaData.i18n.searching + "</div>").show();
 
             this.searchTimeout = setTimeout(function () {
@@ -76,14 +95,87 @@
         },
 
         /**
+         * Position suggestions container relative to field
+         */
+        positionSuggestions: function ($field, $suggestions) {
+            var fieldOffset = $field.offset();
+            var fieldHeight = $field.outerHeight();
+            var fieldWidth = $field.outerWidth();
+
+            $suggestions.css({
+                position: "absolute",
+                top: fieldOffset.top + fieldHeight + "px",
+                left: fieldOffset.left + "px",
+                width: fieldWidth + "px",
+                "z-index": 9999,
+            });
+        },
+
+        /**
          * Get suggestions container for a field
          */
         getSuggestionsContainer: function ($field) {
+            var fieldName = $field.attr("name");
+
             // Try to find in wrapper first (CF7, WPForms)
             var $wrapper = $field.closest(".ndnci-wpaa-field-wrapper, .wpforms-field");
             if ($wrapper.length) {
-                return $wrapper.find(".ndnci-wpaa-suggestions");
+                var $suggestions = $wrapper.find(".ndnci-wpaa-suggestions");
+                if ($suggestions.length) {
+                    return $suggestions;
+                }
             }
+
+            // CF7 might extract elements outside wrapper - look in parent label/p
+            var $parentLabel = $field.closest("label, p, div.field-wrapper");
+            if ($parentLabel.length) {
+                var $parentSuggestions = $parentLabel.find(".ndnci-wpaa-suggestions");
+                if ($parentSuggestions.length) {
+                    return $parentSuggestions;
+                }
+
+                // Look for next element after parent (CF7 structure)
+                var $nextSuggestions = $parentLabel.next(".ndnci-wpaa-suggestions");
+                if ($nextSuggestions.length) {
+                    return $nextSuggestions;
+                }
+            }
+
+            // Look for next sibling suggestions (after wrapper)
+            if ($wrapper.length) {
+                var $nextSuggestions = $wrapper.next(".ndnci-wpaa-suggestions");
+                if ($nextSuggestions.length) {
+                    return $nextSuggestions;
+                }
+            }
+
+            // Last resort - find all suggestions and match by proximity or field name pattern
+            var $allSuggestions = $(".ndnci-wpaa-suggestions");
+
+            if ($allSuggestions.length > 0) {
+                // Try to find the closest one to our field
+                var fieldOffset = $field.offset();
+                var closest = null;
+                var minDistance = Infinity;
+
+                $allSuggestions.each(function () {
+                    var $this = $(this);
+                    var offset = $this.offset();
+                    if (offset) {
+                        var distance =
+                            Math.abs(offset.top - fieldOffset.top) + Math.abs(offset.left - fieldOffset.left);
+                        if (distance < minDistance) {
+                            minDistance = distance;
+                            closest = $this;
+                        }
+                    }
+                });
+
+                if (closest) {
+                    return $(closest);
+                }
+            }
+
             // Fallback to siblings
             return $field.siblings(".ndnci-wpaa-suggestions");
         },
@@ -138,7 +230,11 @@
                     "</div>";
             });
 
-            $suggestions.html(html).show();
+            $suggestions.html(html);
+
+            // Position and show suggestions
+            this.positionSuggestions($field, $suggestions);
+            $suggestions.show();
         },
 
         /**
@@ -147,6 +243,47 @@
         showError: function ($field, message) {
             var $suggestions = this.getSuggestionsContainer($field);
             $suggestions.html('<div class="wpaa-error">' + message + "</div>");
+        },
+
+        /**
+         * Get place ID field for an address field
+         */
+        getPlaceIdField: function ($field) {
+            var fieldName = $field.attr("name");
+            var placeIdName = fieldName + "_place_id";
+
+            // Try in wrapper first
+            var $wrapper = $field.closest(".ndnci-wpaa-field-wrapper, .wpforms-field, .gfield");
+            if ($wrapper.length) {
+                var $placeIdField = $wrapper.find(".ndnci-wpaa-place-id");
+                if ($placeIdField.length) {
+                    return $placeIdField;
+                }
+            }
+
+            // Look in parent
+            var $parent = $field.closest("label, p, div.field-wrapper");
+            if ($parent.length) {
+                var $placeIdField = $parent.find('.ndnci-wpaa-place-id[name="' + placeIdName + '"]');
+                if ($placeIdField.length) {
+                    return $placeIdField;
+                }
+
+                // Look after parent
+                var $nextPlaceId = $parent.next(".ndnci-wpaa-place-id");
+                if ($nextPlaceId.length && $nextPlaceId.attr("name") === placeIdName) {
+                    return $nextPlaceId;
+                }
+            }
+
+            // Search by name anywhere in form
+            var $form = $field.closest("form");
+            var $placeIdByName = $form.find('input[name="' + placeIdName + '"]');
+            if ($placeIdByName.length) {
+                return $placeIdByName;
+            }
+
+            return $();
         },
 
         /**
@@ -165,23 +302,49 @@
             var description = $suggestion.find(".ndnci-wpaa-suggestion-text").text();
 
             // Find the wrapper and field
-            var $wrapper = $suggestion.closest(".ndnci-wpaa-field-wrapper, .wpforms-field, .gfield");
+            var $wrapper = $suggestion.closest(".ndnci-wpaa-field-wrapper, .wpforms-field, .gfield, label, p");
             var $field = $wrapper.find(".ndnci-wpaa-address-field");
-            var $placeIdField = $wrapper.find(".ndnci-wpaa-place-id");
+
+            // If not found in wrapper, the suggestion might be outside - find closest field
+            if ($field.length === 0) {
+                var $allFields = $(".ndnci-wpaa-address-field");
+                var suggestionOffset = $suggestion.offset();
+                var closest = null;
+                var minDistance = Infinity;
+
+                $allFields.each(function () {
+                    var $this = $(this);
+                    var offset = $this.offset();
+                    if (offset) {
+                        var distance = Math.abs(offset.top - suggestionOffset.top);
+                        if (distance < minDistance) {
+                            minDistance = distance;
+                            closest = $this;
+                        }
+                    }
+                });
+
+                if (closest) {
+                    $field = $(closest);
+                }
+            }
+
+            // Find place_id field using helper
+            var $placeIdField = this.getPlaceIdField($field);
 
             $field.val(description);
-            $placeIdField.val(placeId);
+            if ($placeIdField.length) {
+                $placeIdField.val(placeId);
+            }
 
             this.hideSuggestions($field);
 
             // Get place details
             this.getPlaceDetails(placeId, $field);
         },
-
         /**
          * Get place details
-         */
-        getPlaceDetails: function (placeId, $field) {
+         */ getPlaceDetails: function (placeId, $field) {
             var self = this;
 
             $.ajax({
@@ -208,7 +371,9 @@
         initMaps: function () {
             var self = this;
 
-            $(".ndnci-wpaa-map").each(function () {
+            var $maps = $(".ndnci-wpaa-map");
+
+            $maps.each(function () {
                 var $map = $(this);
 
                 if (ndnciWpaaData.provider === "openstreetmap") {
@@ -224,7 +389,6 @@
          */
         initLeafletMap: function ($mapContainer) {
             if (typeof L === "undefined") {
-                console.error("Leaflet library not loaded");
                 return;
             }
 
@@ -246,7 +410,6 @@
          */
         initGoogleMap: function ($mapContainer) {
             if (typeof google === "undefined" || typeof google.maps === "undefined") {
-                console.error("Google Maps library not loaded");
                 return;
             }
 
